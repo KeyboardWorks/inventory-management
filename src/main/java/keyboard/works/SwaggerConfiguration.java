e2498aa8efa25029c0b8776ccfb0e43fa991e34f
Andres Nodas <nodasiniesta@gmail.com>
2021-07-19 22:08:02 +0700

    Implements Swagger

---
+++ src/main/java/keyboard/works/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package keyboard.works;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("keyboard.works.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Inventory Management")
+				.description("Making inventory management (Fifo, Lifo, Averange, Goods Receipt, Goods Issue, etc)")
+				.version("0.0.1")
+				.build();
+	}
+	
+}
